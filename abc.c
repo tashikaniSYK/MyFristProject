61ab7edc2b1ea3109f160d6261c45b28d6c15fca
tashikaniSYK <82259164+tashikaniSYK@users.noreply.github.com>
2021-11-01 18:45:37 +0900

    Create abc.c

---
+++ abc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d%c%d%c%d \n", a,'+',b,'=',c);
+    return 0;
+
+}
